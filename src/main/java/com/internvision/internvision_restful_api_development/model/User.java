2cf58cbe76784d7410849fa457ef43b391e69857
AliJafarli <somagni4icent@gmail.com>
2025-10-02 22:01:41 +0400

    Repo added

---
+++ src/main/java/com/internvision/internvision_restful_api_development/model/User.java
@@ -0,0 +1,27 @@
+package com.internvision.internvision_restful_api_development.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+}
