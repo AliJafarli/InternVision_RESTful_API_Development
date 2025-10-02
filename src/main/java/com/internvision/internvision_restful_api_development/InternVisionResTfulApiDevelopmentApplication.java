c554d343422a17ac91f5f978d3ff6ea8c27cbd4e
AliJafarli <somagni4icent@gmail.com>
2025-10-02 17:14:22 +0400

    1

---
+++ src/main/java/com/internvision/internvision_restful_api_development/InternVisionResTfulApiDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.internvision.internvision_restful_api_development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternVisionResTfulApiDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternVisionResTfulApiDevelopmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/internvision/internvision_restful_api_development/InternVisionResTfulApiDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internvision.internvision_restful_api_development;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternVisionResTfulApiDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
