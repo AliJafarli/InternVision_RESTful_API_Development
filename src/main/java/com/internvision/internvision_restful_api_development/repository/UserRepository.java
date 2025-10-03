0e58db0de78be590a30061de5fd94b8a3094ec4d
AliJafarli <somagni4icent@gmail.com>
2025-10-03 14:20:37 +0400

    service added

--- src/main/java/com/internvision/internvision_restful_api_development/repository/UserRepository.java
+++ src/main/java/com/internvision/internvision_restful_api_development/repository/UserRepository.java
@@ -4,8 +4,12 @@ import com.internvision.internvision_restful_api_development.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
