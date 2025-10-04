1fd3f0a1763cf012eb6834eb566ac8e1ad3b8494
AliJafarli <somagni4icent@gmail.com>
2025-10-04 18:16:31 +0400

    Docker MongoDB added

--- src/main/java/com/internvision/internvision_restful_api_development/model/document/User.java
+++ src/main/java/com/internvision/internvision_restful_api_development/model/document/User.java
@@ -25,4 +25,7 @@ public class User {
     @Email(message = "Email should be valid")
     private String email;
 
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
 }
