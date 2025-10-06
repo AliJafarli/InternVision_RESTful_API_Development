ef3b8ac8ae8b00acac0454631755b929355c0e6f
AliJafarli <somagni4icent@gmail.com>
2025-10-06 14:30:44 +0400

    little fix

--- src/main/java/com/internvision/internvision_restful_api_development/model/constants/ApiErrorMessage.java
+++ src/main/java/com/internvision/internvision_restful_api_development/model/constants/ApiErrorMessage.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ApiErrorMessage {
-    USER_NOT_FOUND_BY_ID("User with ID: %s was not found" ),
+    USER_NOT_FOUND_BY_ID("User not found" ),
     USERNAME_ALREADY_EXISTS("Username: %s already exists" ),
     EMAIL_ALREADY_EXISTS("Email: %s already exists" ),
     ;
